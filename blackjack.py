87f1b610f9dba45d1d689938761d438666b43fcd
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-03-24 16:03:43 -0700

    Mostly done. Some chip logic issues

--- blackjack.py
+++ blackjack.py
@@ -27,7 +27,7 @@ class Deck():
             deck_comp += '\n' + card.__str__()
         return "The deck has: " + deck_comp
 
-    def suffle(self):
+    def shuffle(self):
         random.shuffle(self.deck)
 
     def deal(self):
@@ -53,8 +53,8 @@ class Hand():
             self.aces -= 1
 
 class Chips:
-    def __init__(self, total=100):
-        self.total = total
+    def __init__(self):
+        self.total = 100
         self.bet = 0
 
     def win_bet(self):
@@ -74,3 +74,109 @@ def take_bet(chips):
                 print("You only have {} chips.".format(chips.total))
             else:
                 break
+
+def hit(deck,hand):
+    single_card = deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_aces()
+
+def hit_or_stay(deck, hand):
+    global playing
+
+    while True:
+        x = input('Hit/Stay? Enter h or s: ')
+
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        elif x[0].lower() == 's':
+            print("Player Stays")
+            playing = False
+        else:
+            print("Please enter h or s.")
+            continue
+
+        break
+
+def show_some(player,dealer):
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+
+def show_all(player,dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =",player.value)
+
+def player_busts(player,dealer,chips):
+    print("PLAYER BUST")
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print("PLAYER WINS")
+    chips.win_bet()
+
+def dealer_busts(player, dealer, chips):
+    print("DEALER BUST")
+    chips.win_bet()
+
+def dealer_wins(player, dealer, chips):
+    print("DEALER WINS")
+    chips.lose_bet()
+
+def push(player,dealer):
+    print("Dealer and player tie. PUSH")
+
+while True:
+    print("Let's play Blackjack!")
+
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+
+    take_bet(player_chips)
+
+    show_some(player_hand,dealer_hand)
+
+    while playing:
+        hit_or_stay(deck,player_hand)
+        show_some(player_hand,dealer_hand)
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break
+
+    if player_hand.value <= 21:
+        while dealer_hand.value < player_hand.value:
+            hit(deck,dealer_hand)
+
+        show_all(player_hand,dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+        else:
+            push(player_hand,dealer_hand)
+
+    print('\n Player total chips: {}'.format(player_chips.total))
+
+    new_game = input("Play again? y/n: ")
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print("Thanks for playing.")
+        break
